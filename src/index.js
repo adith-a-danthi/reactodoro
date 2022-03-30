37173a176a015aed365d7717197b988179af3d5a
Adith A Danthi <adith.danthi@gmail.com>
2022-03-30 17:23:56 +0530

    style: add styling, overrides

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import { TasksProvider } from './context/tasks-context';
 
+import './styles/index.css';
+
 ReactDOM.render(
   <React.StrictMode>
     <TasksProvider>
