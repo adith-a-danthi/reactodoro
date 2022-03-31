1411e500e9ed6c19fdec4cf8d1de46aa0b616750
Adith A Danthi <adith.danthi@gmail.com>
2022-04-01 00:36:37 +0530

    chore: update navbar, task item lnks

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
+import { Link } from 'react-router-dom';
+
 export default function Navbar() {
   return (
     <header className="my-8">
       <nav className="navbar rounded-lg">
         <h3 className="nav-title heading-2">
-          <span className="fas fa-stopwatch mr-2"></span>
-          Reactodoro
+          <Link to="/">
+            <span className="fas fa-stopwatch mr-2"></span>
+            Reactodoro
+          </Link>
         </h3>
       </nav>
     </header>
